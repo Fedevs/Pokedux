8000a5eb153cfa9d0843f24a8dcb40650cb88499
Federico <fededevsit@gmail.com>
2023-01-18 19:36:48 -0300

    sanitizing store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import { logger } from '../middlewares';
 import { dataReducer, uiReducer } from './slices';
 
+const actionSanitizer = (action) => {
+  action.type === 'data/setPokemons' && action.payload
+    ? { ...action, payload: '<<LONG_BLOB>>' }
+    : action;
+};
+
 const configureStoreOptions = {
   reducer: { data: dataReducer, ui: uiReducer },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: { actionSanitizer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(logger),
 };
 
 export const store = configureStore(configureStoreOptions);
