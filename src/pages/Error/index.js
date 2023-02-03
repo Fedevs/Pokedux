9495dcbf8c0533f19d32509f91ed9cf7af242fb5
Federico <fededevsit@gmail.com>
2023-02-02 23:39:07 -0300

    placeholder on error and favourite pages

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,11 +1,15 @@
 import { useRouteError } from 'react-router-dom';
+import errorImage from 'statics/temporaryError.png';
+import './ErrorPage.css';
 
 const ErrorPage = () => {
   const error = useRouteError();
-  console.log(error);
   return (
-    <div>
-      error<p>{error.statusText || error.message}</p>
+    <div className="error-page">
+      <img src={errorImage} alt="error" />
+      <p>
+        <b>{error.statusText || error.message}</b>
+      </p>
     </div>
   );
 };
--- src/pages/Favourites/index.js
+++ src/pages/Favourites/index.js
@@ -1,5 +1,13 @@
+import favImage from 'statics/temporaryFavourites.png';
+import './Favourites.css';
+
 const Favourites = () => {
-  return <div>Favourite page </div>;
+  return (
+    <div className="favourites">
+      <h3>Not built yet</h3>
+      <img src={favImage} alt="favs" width={500} />{' '}
+    </div>
+  );
 };
 
 export default Favourites;
