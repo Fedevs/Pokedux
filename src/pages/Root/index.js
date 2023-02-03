0670d1aef9ccb0dc9e443c205cd1e5c7cef60740
Federico <fededevsit@gmail.com>
2023-02-03 08:41:02 -0300

    fix router with gh pages

--- src/pages/Root/index.js
+++ src/pages/Root/index.js
@@ -27,7 +27,7 @@ const Root = () => {
         <Searcher loading={loading} onChange={handleOnChange} />
         <nav className="nav">
           <li>
-            <Link to={favouriteView ? '/' : 'favourites'}>
+            <Link to={favouriteView ? 'pokedux' : 'favourites'}>
               <Button
                 icon={favouriteView ? <HomeOutlined /> : <StarOutlined />}
               />
